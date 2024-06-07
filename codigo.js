417e47092f79956703eeaecca9706dfa871fd2b2
Botelho <120726287+botelho-fabio@users.noreply.github.com>
2024-06-06 21:28:08 -0300

    first commit

---
+++ codigo.js
@@ -0,0 +1,26 @@
+function displayDate() {
+  document.getElementById('resultado').innerHTML = Date()
+}
+/*
+function calculaMedia() {
+  let av1 = document.getElementById('av1').value
+  let av2 = document.document.getElementById('av2').value
+  let av3 = document.getElementById('av3').value
+  media = (av1 + av2 + av3) / 3
+
+  console.log(media)
+  return media
+}
+*/
+function multiplica(a, b) {
+  let resultado = a * b
+  return resultado
+}
+var media = 0
+let n1 = 10
+var n3 = 15
+const n4 = 40
+let n2 = 5
+let m = multiplica(n1, n2)
+
+console.log(m)
